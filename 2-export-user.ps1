a580f6003d5adaa8a52c1736fd2afae22bd7c6bf
Will Spurr <willjspurr@gmail.com>
2023-02-16 12:01:19 -0600

    Update 2-export-user.ps1

--- 2-export-user.ps1
+++ 2-export-user.ps1
@@ -1,21 +1,15 @@
-<# Exports computers from source domain with ldifde. Sets them all to "disabled" so
+<# Exports users from source domain with ldifde. Sets them all to "disabled" so
 they're ready to be imported into the target domain. If you don't disable them
 they can't be imported because you aren't setting passwords.
 
 Settings passwords requires connecting to target DC on LDAPS, which doesn't work
 if you don't have a CA.
 
-Run this as Domain Admin if you have computers that can't be read by normal computers.
-
-This doesn't work. Upon ldifde import kept getting "A required attribute is missing" and "attribute is owned by SAM".
-I did trial and error a bunch and could not figure out what attributes were needed and which weren't for
-group managed service accounts. You can create a quick placeholder for this account by doing:
-New-ADComputer <account name>
-Disable-ADAccount <account name>
+Run this as Domain Admin if you have users that can't be read by normal users.
 #>
 
 param(
-    [string]$ExportFile="serviceaccounts.txt",
+    [string]$ExportFile="users.txt",
 
     [string]$DC="dc1.ad.contoso.com",
 
@@ -27,23 +21,24 @@ If (Test-Path $ExportFile) {
     Remove-Item $ExportFile
 }
 
-Write-Host "Exporting service accounts to file..."
+Write-Host "Exporting users to file..."
 $ExportFileTemp = $ExportFile + ".tmp"
-ldifde -f $ExportFileTemp -s $DC -r "(objectClass=msDS-GroupManagedServiceAccount)" -t 636 -c $SourceDN $TargetDN 
+# Filter out computers: https://confluence.atlassian.com/kb/how-to-write-ldap-search-filters-792496933.html
+ldifde -f $ExportFileTemp -s $DC -r "(&(objectClass=user)(!(objectClass=computer)))" -t 636 -c $SourceDN $TargetDN -l dn,objectClass,description,name,userAccountControl,msDS-SupportedEncryptionTypes,samAccountName
 
-Write-Host "Setting all service accounts to disabled..."
-# set UAC to disabled for all service accounts
+Write-Host "Setting all users to disabled..."
+# set UAC to disabled for all users
 $output = ""
 $userCount = 1
 ForEach ($line in (Get-Content $ExportFileTemp)) {
     If ($line -like "userAccountControl:*") {
-        $line = "userAccountControl: 4098"
+        $line = "userAccountControl: 514"
     }
 
-    # write to file after each user is processed to avoid newlines appearing in the middle of computers
+    # write to file after each user is processed to avoid newlines appearing in the middle of users
         # which generate errors
     If ($line -like "dn:*") {
-        Write-Host "Writing service account $userCount to file..."
+        Write-Host "Writing user $userCount to file..."
         $userCount += 1
         $output >> $ExportFile
         $output = ""
